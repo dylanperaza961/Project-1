44444f5258a4d9de6fec3bcf642b141fd401c06c
dylanperaza961 <42747481+dylanperaza961@users.noreply.github.com>
2019-01-30 13:20:33 -0700

    Added all files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> music = new List<string>();
+            while (true)
+            {
+
+                Console.WriteLine("What is the command?");
+                string command = Console.ReadLine();
+                if (command == "add")
+                {
+                    //add to list
+                    Console.WriteLine("What is the song?");
+                    string song = Console.ReadLine();
+                    music.Add(song);
+                    Console.WriteLine($"The song {song} was added");
+                }
+                if (command == "list")
+                {
+                    //shows list
+                    music.ForEach(Console.WriteLine) ;
+                }
+                if (command == "remove")
+                {
+                    Console.WriteLine("Which song would you like to remove?");
+                    string song = Console.ReadLine();
+                    if (music.Contains(song))
+                    {
+                        music.Remove(song);
+                        Console.WriteLine($"The song {song} was removed");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The song {song} was not found");
+                    }
+                }
+                if (command == "quit")
+                {
+                    break;
+                }
+    }
+        }
+    }
+}
